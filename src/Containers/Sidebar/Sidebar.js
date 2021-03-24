8ae8d2bb82bc630b3725f9a6cd28c14c7053e8e8
Jahongir95 <sjahongir0032@gmail.com>
2021-03-24 11:01:26 +0500

    Logos is updated and linked to the sidebar

--- src/Containers/Sidebar/Sidebar.js
+++ src/Containers/Sidebar/Sidebar.js
@@ -1,11 +1,22 @@
+import {
+    MainLogo,
+    HomeIcon,
+    DiscountIcon,
+    PieChartIcon,
+    MailIcon,
+    BillIcon,
+    SettingsIcon,
+    ExitIcon
+} from '../../Components/SidebarIcons/SidebarIcons';
+
 import './Sidebar.scss'
 
 const Sidebar = () => {
 
     return(
         <div className="sidebar">
-
-            <h1>Sidebar</h1>
+            <MainLogo/>
+           <h1>Sidebar</h1>
         </div>
     )
 }
