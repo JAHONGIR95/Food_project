27dff4effd895f7ead3c4966f8401880dc8d76d5
Jahongir95 <sjahongir0032@gmail.com>
2021-03-26 02:10:52 +0500

    settings page is started developing

---
+++ src/Components/Settings-options.js
@@ -0,0 +1,13 @@
+const SettingsOptions = () => {
+    return (
+        <div className="settings-options">
+            <img src={icons} alt=""/>
+            <div className="settings-options-right">
+                <p className="options-title">{optionsTitle}</p>
+                <p className="options-discreption">optionsDiscreption</p>
+            </div>
+        </div>
+    )
+}
+
+export default SettingsOptions;
--- src/Pages/Settings.js
+++ src/Pages/Settings.js
@@ -1,9 +1,15 @@
+import Header from '../Containers/Header/Header';
+
 import './Pages.scss';
 
 const Settings = () => {
     return (
         <div className="settings">
-            <h1>Settings</h1>
+            <Header headerName="Settings"/>
+            <div className="settings-main">
+                <div className="settings-main-left"></div>
+                <div className="settings-main-right"></div>
+            </div>
         </div>
     )
 }
